0d7ace7c37e258f6a6d1bca57469e71907f63cc7
eric <eirik@autopay.io>
2024-09-28 21:01:15 +0200

    init

---
+++ crates/propane_lexer/src/lib.rs
@@ -0,0 +1,161 @@
+use codespan::Span;
+
+mod scanner;
+pub fn tokenize(src: &str) -> Vec<Token> {
+    scanner::scan(&src).collect()
+}
+
+#[derive(Debug)]
+pub struct Token {
+    kind: TokenKind,
+    span: Span,
+}
+
+#[derive(Debug)]
+pub enum TokenKind {
+    // Multi-char tokens:
+    /// "// comment"
+    LineComment,
+
+    /// `/* block comment */`
+    ///
+    /// Block comments can be recursive, so a sequence like `/* /* */`
+    /// will not be considered terminated and will result in a parsing error.
+    BlockComment,
+
+    /// Any whitespace character sequence.
+    Whitespace,
+
+    /// "ident"
+    Ident,
+
+    /// Like the above, but containing invalid unicode codepoints.
+    InvalidIdent,
+
+    /// Examples: `12u8`, `1.0e-40`, `b"123"`. Note that `_` is an invalid
+    /// suffix, but may be present here on string and float literals. Users of
+    /// this type will need to check for and reject that case.
+    ///
+    /// See [LiteralKind] for more details.
+    Literal {
+        kind: LiteralKind,
+    },
+
+    // Two-char tokens:
+    BangEq,
+    EqEq,
+    GtEq,
+    LtEq,
+
+    // One-char tokens:
+    /// ";"
+    Semi,
+    /// ","
+    Comma,
+    /// "."
+    Dot,
+    /// "("
+    OpenParen,
+    /// ")"
+    CloseParen,
+    /// "{"
+    OpenBrace,
+    /// "}"
+    CloseBrace,
+    /// "["
+    OpenBracket,
+    /// "]"
+    CloseBracket,
+    /// "@"
+    At,
+    /// "#"
+    Pound,
+    /// "~"
+    Tilde,
+    /// "?"
+    Question,
+    /// ":"
+    Colon,
+    /// "$"
+    Dollar,
+    /// "="
+    Eq,
+    /// "!"
+    Bang,
+    /// "<"
+    Lt,
+    /// ">"
+    Gt,
+    /// "-"
+    Minus,
+    /// "&"
+    And,
+    /// "|"
+    Or,
+    /// "+"
+    Plus,
+    /// "*"
+    Star,
+    /// "/"
+    Slash,
+    /// "^"
+    Caret,
+    /// "%"
+    Percent,
+
+    /// Unknown token, not expected by the lexer, e.g. "№"
+    Unknown,
+
+    /// End of input.
+    Eof,
+}
+
+/// Enum representing the literal types supported by the lexer.
+///
+/// Note that the suffix is *not* considered when deciding the `LiteralKind` in
+/// this type. This means that float literals like `1f32` are classified by this
+/// type as `Int`. (Compare against `rustc_ast::token::LitKind` and
+/// `rustc_ast::ast::LitKind`).
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum LiteralKind {
+    /// "12_u8", "0o100", "0b120i99", "1f32".
+    // Int { base: Base, empty_int: bool },
+    Int,
+    /// "12.34f32", "1e3", but not "1f32".
+    // Float { base: Base, empty_exponent: bool },
+    Float,
+    /// "'a'", "'\\'", "'''", "';"
+    Char { terminated: bool },
+    // /// "b'a'", "b'\\'", "b'''", "b';"
+    // Byte { terminated: bool },
+    /// ""abc"", ""abc"
+    Str { terminated: bool },
+}
+
+#[cfg(test)]
+mod tests {
+    use codespan::Files;
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut files = Files::new();
+
+        let main = files.add(
+            "main",
+            r#"
+            "A string!"
+// this is a comment
+(( )){} // grouping stuff
+!*+-/=<> <= == // operators
+        "#,
+        );
+
+        let tokens = tokenize(files.source(main));
+
+        for token in &tokens {
+            let _ = dbg!(&token.kind, files.source_slice(main, token.span).unwrap());
+        }
+    }
+}
---
+++ crates/propane_lexer/src/scanner.rs
@@ -0,0 +1,156 @@
+use std::iter;
+use std::str::Chars;
+
+use codespan::Span;
+
+use crate::TokenKind::*;
+use crate::{Token, TokenKind};
+
+struct Scanner<'src> {
+    source: Chars<'src>,
+    length: u32,
+    char_index: u32,
+}
+
+impl Scanner<'_> {
+    fn next_token(&mut self) -> Option<Token> {
+        let char = self.source.next()?;
+
+        let start = self.char_index;
+
+        let kind = match char {
+            c if is_whitespace(c) => {
+                self.eat_while(is_whitespace);
+
+                Whitespace
+            }
+            ',' => Comma,
+            '.' => Dot,
+            '(' => OpenParen,
+            ')' => CloseParen,
+            '{' => OpenBrace,
+            '}' => CloseBrace,
+            '[' => OpenBracket,
+            ']' => CloseBracket,
+            '@' => At,
+            '#' => Pound,
+            '~' => Tilde,
+            '?' => Question,
+            ':' => Colon,
+            '$' => Dollar,
+            '-' => Minus,
+            '&' => And,
+            '|' => Or,
+            '+' => Plus,
+            '*' => Star,
+            '^' => Caret,
+            '%' => Percent,
+            '!' => self.match_advance_or('=', BangEq, Bang),
+            '=' => self.match_advance_or('=', EqEq, Eq),
+            '<' => self.match_advance_or('=', LtEq, Lt),
+            '>' => self.match_advance_or('=', GtEq, Gt),
+            '/' => match self.peek() {
+                '/' => {
+                    self.eat_while(|ch| ch != '\n');
+
+                    LineComment
+                }
+                // '*' => self.block_comment(),
+                _ => Slash,
+            },
+            // Numeric literal.
+            '0'..='9' => TokenKind::Literal {
+                kind: crate::LiteralKind::Float,
+            },
+            '"' => {
+                self.eat_while(|ch| ch != '"');
+
+                let terminated = self.peek() == '"';
+
+                let lit = Literal {
+                    kind: crate::LiteralKind::Str { terminated },
+                };
+
+                if terminated {
+                    self.discard();
+                }
+
+                lit
+            }
+            _ => Unknown,
+        };
+
+        let end = self.length - self.source.as_str().len() as u32;
+
+        let span = Span::new(start, end);
+
+        self.char_index = end;
+
+        Some(Token { kind, span })
+    }
+
+    fn match_advance_or(&mut self, ch: char, is: TokenKind, or: TokenKind) -> TokenKind {
+        if self.peek() == ch {
+            self.discard();
+            is
+        } else {
+            or
+        }
+    }
+
+    fn peek(&self) -> char {
+        self.source.clone().next().unwrap_or('\0')
+    }
+
+    fn discard(&mut self) {
+        self.source.next();
+    }
+
+    pub(crate) fn eat_while(&mut self, mut predicate: impl FnMut(char) -> bool) {
+        while predicate(self.peek()) && self.peek() != '\0' {
+            self.discard();
+        }
+    }
+}
+
+pub fn scan(src: &str) -> impl Iterator<Item = Token> + '_ {
+    let mut scanner = Scanner {
+        source: src.chars(),
+        char_index: 0,
+        length: src.len() as u32,
+    };
+
+    iter::from_fn(move || scanner.next_token()).chain(iter::once(Token {
+        kind: TokenKind::Eof,
+        span: Span::new(src.len() as u32, src.len() as u32),
+    }))
+}
+
+fn is_whitespace(c: char) -> bool {
+    // This is Pattern_White_Space.
+    //
+    // Note that this set is stable (ie, it doesn't change with different
+    // Unicode versions), so it's ok to just hard-code the values.
+
+    matches!(
+        c,
+        // Usual ASCII suspects
+        '\u{0009}'   // \t
+        | '\u{000A}' // \n
+        | '\u{000B}' // vertical tab
+        | '\u{000C}' // form feed
+        | '\u{000D}' // \r
+        | '\u{0020}' // space
+
+        // NEXT LINE from latin1
+        | '\u{0085}'
+
+        // Bidi markers
+        | '\u{200E}' // LEFT-TO-RIGHT MARK
+        | '\u{200F}' // RIGHT-TO-LEFT MARK
+
+        // Dedicated whitespace characters from Unicode
+        | '\u{2028}' // LINE SEPARATOR
+        | '\u{2029}' // PARAGRAPH SEPARATOR
+    )
+}
